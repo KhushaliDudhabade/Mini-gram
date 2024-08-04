5c663b7ee142f87300063678ff26945ab427f805
Khushali Dudhabade <khushalidudhabade27@gmail.com>
2024-08-04 10:23:33 +0530

    files uploaded

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
+import { HashRouter as Router } from 'react-router-dom';
 import './App.css';
 
 ReactDOM.render(
